bd6d97492854336fdee0e3cc6beb3fa7912d2f84
Ali <alhart@redhat.com>
2023-02-26 17:16:57 -0500

    Initial Pipeline Release

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+"""
+Performs basic cleaning on the data and saves the clean data in Weights and Biases
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+    logger.info("Running the basic_cleaning step")
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    
+    logger.info(f"Getting {args.input_artifact} from W&B")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    
+    df = pd.read_csv(artifact_local_path)
+
+    #min_price = 10
+    #max_price = 350
+    
+    logger.info("Cleaning data...")
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    logger.info(f"Saving clean data to .csv (clean_sample.csv)")
+    df.to_csv("clean_sample.csv", index=False)
+    
+    logger.info("Creating W&B artifact and adding clean_sample.csv.")
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,        
+    )
+    
+    artifact.add_file("clean_sample.csv")
+    
+    logger.info("Uploading artifact")
+    run.log_artifact(artifact)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="This step cleans the data")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help="Name of the input artifact to clean",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help="Name of the output artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help="Output artifact type",
+        required=True
+    )
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="Description of the output artifact",
+        required=True
+    )
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="Minimum rental price",
+        required=True
+    )
+    
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="Maximum rental price",
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    go(args)
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,17 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    """
+    Test that dataset size is reasonable. 
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Test that all prices are between min and max prices. 
+    """
+    assert data['price'].dropna().between(min_price, max_price).all(), (
+        f"Prices failed the test. Should be between {min_price} and {max_price}, "
+        f"instead min={data['price'].min()} and max={data['price'].max()}"
+    )
